9f9afb0857abcdb8b34c0988616a69f36d171b09
akshayxemo <akshaykrdas001@gmail.com>
2022-02-28 13:19:38 +0530

    date fns module added

--- src/components/BloodBankCard.vue
+++ src/components/BloodBankCard.vue
@@ -1,31 +1,18 @@
 <template>
-  <div class="wrapper p-4 rounded-lg m-4 border-2 border-slate-400/70">
-    <div class="tags flex gap-2">
-      <span class="py-1 px-2 border-2 border-red-500 text-red-500 font-semibold rounded-md uppercase">Private</span>
-      <span class="py-1 px-2 border-2 border-red-500 text-red-500 font-semibold rounded-md uppercase">Verfied</span>
+  <div class="grid grid-rows-3 grid-cols-3 p-4 rounded-lg m-auto border-2 border-slate-400/70 w-3/4 h-1/6">
+    <div class="row-start-1 row-span-1 col-start-1 col-span-1 flex gap-2">
+      <span class="py-1 px-2 border-2 border-red-500 text-red-500 font-semibold rounded-md uppercase h-max" v-for="tag in tags" :key="tag">{{tag}}</span>
     </div>
-    <span class="update">Last Updated: {{ lastUpdate }}</span>
-    <div class="info">
+    <span class="row-start-1 row-span-1 col-start-3 col-span-1 update"><em>Last Updated &nbsp;</em> {{ lastUpdate }}</span>
+    <div class="row-start-2 row-span-1 col-start-1 col-span-2">
       <h1 class="text-3xl font-bold my-3">{{ name }}</h1>
       <h3 class="text-lg">{{ address }}, {{ pincode }}</h3>
     </div>
     <button
-      @click="getDirection"
-      class="
-        px-4
-        py-2
-        font-semibold
-        bg-red-500
-        hover:bg-red-600
-        text-white
-        rounded-lg
-        shadow-md
-        w-max
-      "
-    >
+      class="row-start-2 row-span-1 col-start-3 col-span-1 px-4 py-2 font-semibold bg-red-500 hover:bg-red-600 text-white rounded-lg shadow-md w-max h-max">
       Get Direction
     </button>
-    <section class="supplies border-t-2 border-dashed border-black/30 mt-4">
+    <section class="row-start-3 row-span-1 col-start-1 col-span-3 border-t-2 border-dashed border-black/30 mt-4">
       <h4 class="font-bold text-lg my-2">Available Supplies</h4>
       <ul class="mt-2 flex gap-4">
         <li class="text-xl flex flex-col" v-for="stock in stocks" :key="stock.type">
@@ -41,7 +28,6 @@
             </div>
           </div>
         </li>
-        
       </ul>
     </section>
   </div>
@@ -49,12 +35,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { formatDistance } from 'date-fns'
 
 export default defineComponent({
   name: "BloodBankCard",
   data() {
     return {
-      isShow: false,
+      tags: ["govt", "verified"],
       name: "Subhashgram General Hospital",
       address: "Subhasgram, Kolkata, West Bengal",
       pincode: 700147,
@@ -64,37 +51,19 @@ export default defineComponent({
         {type: "AB", units:{ "+": 123, "-": 2 }},
         {type: "O", units:{ "+": 28, "-": 0 }},
       ],
-      lastUpdate: new Date(),
+      lastUpdateDate: new Date(2022,1,27,18,28),
     };
   },
+  computed: {
+    lastUpdate(){
+      return formatDistance(this.lastUpdateDate, new Date(), { addSuffix: true })
+    }
+  },
   methods: {
-    getDirection() {
-      this.isShow = !this.isShow;
-    },
   },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.wrapper {
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-
-  .tags {
-    grid-row: 1 / span 1;
-  }
-
-  .update {
-    grid-row: 1 / span 1;
-  }
-
-  .info {
-    grid-row: 2 / span 1;
-  }
-
-  .supplies {
-    grid-row: 3 / span 1;
-  }
-}
 </style>
